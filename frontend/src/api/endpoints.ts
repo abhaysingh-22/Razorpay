d5ae97ea3ad8e1045174980858237fad21e1e4dd
abhaysingh-22 <abhaysingh6575@gmail.com>
2026-08-25 14:40:50 +0530

    Add API client and endpoints for transaction management

---
+++ frontend/src/api/endpoints.ts
@@ -0,0 +1,32 @@
+import api from "./client";
+import { Transaction, RecoveryAttempt, BatchSummary } from "../types";
+
+export const getTransactions = async (status?: string) => {
+  const res = await api.get<Transaction[]>("/transactions/", { params: { status } });
+  return res.data;
+};
+
+export const getTransactionDetail = async (id: string) => {
+  const res = await api.get(`/transactions/${id}`);
+  return res.data as { transaction: Transaction; recovery_attempts: RecoveryAttempt[] };
+};
+
+export const getLatestSummary = async () => {
+  const res = await api.get<BatchSummary>("/metrics/summary");
+  return res.data;
+};
+
+export const getSummaryHistory = async () => {
+  const res = await api.get<BatchSummary[]>("/metrics/history");
+  return res.data;
+};
+
+export const runRecoveryBatch = async () => {
+  const res = await api.post("/recovery/run");
+  return res.data;
+};
+
+export const getRecoveryAttempts = async () => {
+  const res = await api.get<RecoveryAttempt[]>("/recovery/attempts");
+  return res.data;
+};
